a4ea320c07ceb0fb48fc65cab62ca23a8b55453c
Markus Wind <markuswind@gmail.com>
2016-01-14 17:52:55 +0100

    add selectstate to change tableviewcells on the fly

--- hanze-ov-ios/viewcontrollers/SelectTableViewController.swift
+++ hanze-ov-ios/viewcontrollers/SelectTableViewController.swift
@@ -8,18 +8,22 @@
 
 import UIKit
 
+enum SelectState {
+    case Institute
+    case Class
+}
+
 class SelectTableViewController: UITableViewController {
 
-    override func viewDidLoad() {
-        tableView.registerNib(UINib(nibName: "InstituteOptionCell", bundle: nil), forCellReuseIdentifier: "InstituteOptionCell")
+    var selectState: SelectState = .Class
 
-        styleTableview()
+    override func viewDidLoad() {
+        themeTableView()
+        setupTableView()
     }
 
-    private func styleTableview() {
-        tableView.rowHeight = 65.0
-
-        tableView.backgroundColor = UIColor(red:0.97, green:0.77, blue:0.36, alpha:1)
+    private func themeTableView() {
+        tableView.backgroundColor = UIColor(red:0.96, green:0.82, blue:0.4, alpha:1)
         tableView.separatorColor = UIColor(red: 0.89, green: 0.48, blue:0.49, alpha:1)
 
         tableView.layer.borderWidth = 0.5
@@ -27,14 +31,51 @@ class SelectTableViewController: UITableViewController {
         tableView.layer.borderColor = UIColor(red:0.89, green:0.48, blue:0.49, alpha:1).CGColor
     }
 
+    private func setupTableView() {
+        switch selectState {
+        case .Institute:
+            setupInstituteSelection()
+        case .Class:
+            setupClassSelection()
+        }
+    }
+
+    private func setupInstituteSelection() {
+        tableView.registerNib(UINib(nibName: "InstituteOptionCell", bundle: nil), forCellReuseIdentifier: "InstituteOptionCell")
+        tableView.rowHeight = 65.0
+    }
+
+    private func setupClassSelection() {
+        tableView.registerNib(UINib(nibName: "ClassOptionCell", bundle: nil), forCellReuseIdentifier: "ClassOptionCell")
+        tableView.rowHeight = 55.0
+    }
+
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        switch selectState {
+        case .Institute:
+            return 10
+        case .Class:
+            return 30
+        }
     }
 
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("InstituteOptionCell", forIndexPath: indexPath) as! InstituteOptionCell
+        var cell: UITableViewCell
+
+        switch selectState {
+        case .Institute:
+            cell = tableView.dequeueReusableCellWithIdentifier("InstituteOptionCell", forIndexPath: indexPath) as! InstituteOptionCell
+        case .Class:
+            cell = tableView.dequeueReusableCellWithIdentifier("ClassOptionCell", forIndexPath: indexPath) as! ClassOptionCell
+        }
 
         return cell
+    }
+
+    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
+        print("indexPath: \(indexPath.row)")
+
+        return indexPath
     }
 
 }
@@ -44,6 +85,33 @@ class InstituteOptionCell: UITableViewCell {
     @IBOutlet var iconImage: UIImageView!
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
+
+    var id: String?
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        let selectedBackgroundColorView = UIView()
+        selectedBackgroundColorView.backgroundColor = UIColor(red:0.96, green:0.82, blue:0.4, alpha:0.8)
+
+        selectedBackgroundView = selectedBackgroundColorView
+        backgroundColor = UIColor(red:0.97, green:0.77, blue:0.36, alpha:1)
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+
+}
+
+class ClassOptionCell: UITableViewCell {
+
+    @IBOutlet var yearLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
 
     var id: String?
 
