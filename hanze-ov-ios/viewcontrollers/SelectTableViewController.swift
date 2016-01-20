46122b7b5292f888f0b692560d4ba430fa2f8259
Markus Wind <markuswind@gmail.com>
2016-01-20 19:28:13 +0100

    start classcells

--- hanze-ov-ios/viewcontrollers/SelectTableViewController.swift
+++ hanze-ov-ios/viewcontrollers/SelectTableViewController.swift
@@ -18,11 +18,11 @@ class SelectTableViewController: UITableViewController {
 
     var selectState: SelectState = .Institute
     var instituteOptions: JSON = []
-    var scheduleOptions: JSON = []
+    var classOptions: JSON = []
 
     override func viewDidLoad() {
         themeTableView()
-        setupTableView()
+        setupInstituteSelection()
     }
 
     private func themeTableView() {
@@ -32,15 +32,6 @@ class SelectTableViewController: UITableViewController {
         tableView.layer.borderWidth = 0.5
         tableView.layer.cornerRadius = 4.0
         tableView.layer.borderColor = UIColor(red: 0.89, green: 0.48, blue: 0.49, alpha: 1).CGColor
-    }
-
-    private func setupTableView() {
-        switch selectState {
-        case .Institute:
-            setupInstituteSelection()
-        case .Class:
-            setupClassSelection()
-        }
     }
 
     private func setupInstituteSelection() {
@@ -56,27 +47,12 @@ class SelectTableViewController: UITableViewController {
         reloadTableData()
     }
 
-    private func setupClassSelection() {
-        tableView.registerNib(UINib(nibName: "ClassOptionCell", bundle: nil), forCellReuseIdentifier: "ClassOptionCell")
-        tableView.rowHeight = 55.0
-
-        reloadTableData()
-    }
-
-    private func reloadTableData() {
-        UIView.transitionWithView(tableView, duration:0.65, options:.TransitionCrossDissolve,
-            animations: { () -> Void in
-                self.tableView.reloadData()
-            },
-        completion: nil)
-    }
-
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch selectState {
         case .Institute:
             return instituteOptions.count
         case .Class:
-            return scheduleOptions.count
+            return classOptions.count
         }
     }
 
@@ -90,12 +66,21 @@ class SelectTableViewController: UITableViewController {
             let key: AnyObject = Array(self.instituteOptions.dictionaryValue.keys)[indexPath.row]
             let value = self.instituteOptions[key as! String]
 
+            instituteOptionCell.groupId = key as? String
             instituteOptionCell.nameLabel.text = value["code"].stringValue
             instituteOptionCell.descriptionLabel.text = value["name"].stringValue
 
             cell = instituteOptionCell
         case .Class:
-            cell = tableView.dequeueReusableCellWithIdentifier("ClassOptionCell", forIndexPath: indexPath) as! ClassOptionCell
+            let classOptionCell = tableView.dequeueReusableCellWithIdentifier("ClassOptionCell", forIndexPath: indexPath) as! ClassOptionCell
+
+            let key: AnyObject = Array(self.classOptions.dictionaryValue.keys)[indexPath.row]
+            let value = self.classOptions[key as! String]
+
+            print(key)
+            print(value)
+
+            cell = classOptionCell
         }
 
         styleTableCell(cell, row: indexPath.row)
@@ -122,7 +107,9 @@ class SelectTableViewController: UITableViewController {
         case .Institute:
             selectState = .Class
 
-            setupTableView()
+            let selectedCell = tableView.cellForRowAtIndexPath(indexPath) as! InstituteOptionCell
+
+            setupClassSelection(selectedCell.groupId!)
             updateNavigationBar()
             
             break
@@ -134,6 +121,27 @@ class SelectTableViewController: UITableViewController {
         }
 
         return indexPath
+    }
+
+    private func setupClassSelection(groupId: String) {
+        tableView.registerNib(UINib(nibName: "ClassOptionCell", bundle: nil), forCellReuseIdentifier: "ClassOptionCell")
+        tableView.rowHeight = 55.0
+
+        Client.sharedClient.performRequestWithMethod(.GET, path: "/group/" + groupId, parameters: nil, completion: fillClassOptions)
+    }
+
+    private func fillClassOptions(result: JSON) {
+        classOptions = result
+
+        reloadTableData()
+    }
+
+    private func reloadTableData() {
+        UIView.transitionWithView(tableView, duration:0.65, options:.TransitionCrossDissolve,
+            animations: { () -> Void in
+                self.tableView.reloadData()
+            },
+        completion: nil)
     }
 
     // FIXME:: this ain't working (yet)
@@ -156,7 +164,7 @@ class InstituteOptionCell: UITableViewCell {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
 
-    var id: String?
+    var groupId: String?
 
     override func awakeFromNib() {
         super.awakeFromNib()
